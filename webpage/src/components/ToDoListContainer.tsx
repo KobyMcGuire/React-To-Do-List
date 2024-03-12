deb7dc4c3250743fbc6f753cd36dc96a2710b485
KobyMcGuire <mcguirej2019@gmail.com>
2024-03-12 19:27:00 -0400

    Put modal code in to to do list container

--- webpage/src/components/ToDoListContainer.tsx
+++ webpage/src/components/ToDoListContainer.tsx
@@ -25,16 +25,69 @@ export const ToDoListContainer = () => {
       });
   });
 
+  function showToDoItems() {}
+
   return (
     <>
       <h1 className="text-center">To-Do List</h1>
       <ul className="list-group">
         {toDoLists.map((toDoList) => (
-          <li className="list-group-item" key={toDoList.id}>
+          <li
+            onClick={showToDoItems}
+            className="list-group-item"
+            key={toDoList.id}
+          >
             {toDoList.title}
+
+            <button
+              type="button"
+              className="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target="#toDoModal"
+            >
+              {" "}
+              View To-Do List Items
+            </button>
           </li>
         ))}
       </ul>
+
+      <div
+        className="modal fade"
+        id="toDoModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                To Do Items
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
